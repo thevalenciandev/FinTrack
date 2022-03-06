65f7534e084cd75fedfcb047add9c5b288b8a7e1
thevalenciandev <thevalenciandev@gmail.com>
2022-03-06 15:40:14 +0000

    Initial commit with basic Spring Boot app starting up

---
+++ src/test/java/com/thevalenciandev/fintrack/FinTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thevalenciandev.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
